2a2cf77d10721854708fa429343e332140f77bd7
AdenKoperczak <awkoperczak@gmail.com>
2024-10-11 16:24:57 -0400

    Merge newer location-marker into location_marker_part2

--- scwx-qt/source/scwx/qt/types/marker_types.cpp
+++ scwx-qt/source/scwx/qt/types/marker_types.cpp
@@ -14,15 +14,7 @@ const std::vector<MarkerIconInfo>& getMarkerIcons()
    {
       markerIcons = {
          MarkerIconInfo(types::ImageTexture::LocationMarker, -1, -1),
-         MarkerIconInfo(types::ImageTexture::LocationPinBlack, 8, 23),
-         MarkerIconInfo(types::ImageTexture::LocationPinBlue, 8, 23),
-         MarkerIconInfo(types::ImageTexture::LocationPinGreen, 8, 23),
-         MarkerIconInfo(types::ImageTexture::LocationPinMagenta, 8, 23),
-         MarkerIconInfo(types::ImageTexture::LocationPinOrange, 8, 23),
-         MarkerIconInfo(types::ImageTexture::LocationPinPink, 8, 23),
-         MarkerIconInfo(types::ImageTexture::LocationPinRed, 8, 23),
-         MarkerIconInfo(types::ImageTexture::LocationPinWhite, 8, 23),
-         MarkerIconInfo(types::ImageTexture::LocationPinYellow, 8, 23),
+         MarkerIconInfo(types::ImageTexture::LocationPin, 8, 23),
          MarkerIconInfo(types::ImageTexture::LocationHouse, -1, -1),
       };
    }
