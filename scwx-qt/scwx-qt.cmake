cb56a6c5b287ca4989dbb5cf9317097c0993ab11
AdenKoperczak <awkoperczak@gmail.com>
2024-10-22 11:15:38 -0400

    change variable set to the correct version for external setting

--- scwx-qt/scwx-qt.cmake
+++ scwx-qt/scwx-qt.cmake
@@ -10,7 +10,7 @@ set(CMAKE_AUTORCC ON)
 
 set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
-set(__QT_DEPLOY_VERBOSE ON)
+set(QT_ENABLE_VERBOSE_DEPLOYMENT ON)
 
 find_package(Boost)
 find_package(Fontconfig)
