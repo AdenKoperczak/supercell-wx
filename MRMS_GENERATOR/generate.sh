aac8ca75c49bdec52856da7295e2ad745e337928
AdenKoperczak <awkoperczak@gmail.com>
2025-01-15 16:30:53 -0500

    add comments and fix a few mistakes in generate.sh

--- MRMS_GENERATOR/generate.sh
+++ MRMS_GENERATOR/generate.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-#gdal_translate "/vsigzip//vsicurl/https://mrms.ncep.noaa.gov/2D/MergedBaseReflectivity/MRMS_MergedBaseReflectivity.latest.grib2.gz" \
-#               test_dataset.mbtiles -of MBTILES
+# pull the latest MRMS reflectivity data and convert it to a GeoTiff using a
+# palette (default palette). gdaldem does not convert between projections, and
+# gdal_translate does not allow for the color translation, so color translation
+# needs to happen first. mbtiles must be in mercader projection, so it cannot
+# convert directly to mbtiles, so geotiff is used as an itermediate format
 gdaldem color-relief \
     "/vsigzip//vsicurl/https://mrms.ncep.noaa.gov/2D/MergedBaseReflectivity/MRMS_MergedBaseReflectivity.latest.grib2.gz" \
     palette.txt test_dataset.geotiff  -alpha -of GTiff
+# convert the geotiff to a mbtiles file including reprojection
 gdal_translate test_dataset.geotiff test_dataset.mbtiles -of MBTILES
-gdaladdo -r average test_dataset.mbtiles 4 8 16 32 128
+# create tiles of diffrent resolutions for multiple zoom levels
+gdaladdo -r average test_dataset.mbtiles 2 4 8 16 32
