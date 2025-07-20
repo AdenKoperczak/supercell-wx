84264aef2b365cfddb71f8a045de2cf584b314a9
AdenKoperczak <awkoperczak@gmail.com>
2025-07-20 09:34:40 -0400

    add logging to stopbuild so I can tell why it is failing

--- tools/conan/stopbuild/conanfile.py
+++ tools/conan/stopbuild/conanfile.py
@@ -10,6 +10,9 @@ class StopBuildConan(ConanFile):
         if os.getenv("CONAN_NO_BUILD", "0") == "1":
             self.output.info("Build aborted: CONAN_NO_BUILD set")
             sys.exit(1)
+        else:
+            self.output.info("Build continueing: CONAN_NO_BUILD not set")
+
 
     def package(self):
         pass
